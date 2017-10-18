fd7b17cbc1272f3234cbba3c60a6fde965589b3c
Shealan Forshaw <shealan@plasticated.com>
2017-10-18 12:40:09 +1100

    Added Vessel and Passport stuff

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class)->create([
+            'name' => 'Example User',
+            'email' => 'example@example.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
